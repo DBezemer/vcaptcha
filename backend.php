0ec27c72035d8052ace46e5ef11c050b9dd8da72
David Bezemer <david.bezemer@uplearning.nl>
2014-06-15 21:06:45 +0200

    Add sort and search to backend

--- backend.php
+++ backend.php
@@ -1,3 +1,17 @@
+<html>
+<head>
+<link href="jquery.dataTables.min.css" rel="stylesheet"> 
+<script src="jquery.js"></script>
+<script src="jquery.dataTables.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('#backend').dataTable();
+} );
+</script>
+</head>
+<body>
+
+
 <?php
 $con=mysqli_connect("localhost","vcaptcha","pw4vcaptcha","vcaptcha");
 // Check connection
@@ -7,19 +21,21 @@ if (mysqli_connect_errno()) {
 
 $result = mysqli_query($con,"SELECT * FROM vcaptcha");
 
-echo "<table border='1'>
+echo "<table border='1' id='backend'>
+<thead>
 <tr>
 <th>id</th>
 <th>entry</th>
 <th>start time</th>
 <th>duration</th>
 <th>transcript</th>
-</tr>";
+</tr>
+</thead>";
 
 while($row = mysqli_fetch_array($result)) {
   echo "<tr>";
   echo "<td>" . $row['id'] . "</td>";
-  echo "<td>" . $row['entry'] . "</td>";
+  echo "<td>" . $row['entryid'] . "</td>";
   echo "<td>" . $row['starttime'] . "</td>";
   echo "<td>" . $row['duration'] . "</td>";
   echo "<td>" . $row['transcript'] . "</td>";
@@ -30,3 +46,5 @@ echo "</table>";
 
 mysqli_close($con);
 ?>
+</body>
+</html>
